bba97f0e8663ecd33639a60199dc3b4f29496a93
Jeremy Echols <jechols@uoregon.edu>
2016-01-21 08:58:31 -0800

    Allow external file to override sensitive settings

--- settings.py
+++ settings.py
@@ -1,5 +1,36 @@
+import ConfigParser
+
+def override_db(conf):
+  for key in ("ENGINE", "HOST", "PORT", "NAME", "USER", "PASSWORD"):
+    if conf.has_option("database", key):
+      DATABASES['default'][key] = conf.get("database", key)
+
+def override_solr(conf):
+  if conf.has_option("solr", "URL"):
+    SOLR = conf.get("solr", "URL")
+
+def override_secrets(conf):
+  if conf.has_option("secrets", "SECRET_KEY"):
+    SECRET_KEY = conf.get("secrets", "SECRET_KEY")
+
 # Pull in the base settings
 from settings_base import *
 
-# Pull in overrides for things like DB, solr, etc.  THIS LINE MUST BE LAST!
+# Pull in overrides for things like DB, solr, etc.  This line MUST come after
+# the base settings!
 from settings_local import *
+
+# Allow for environment overrides in /etc/openoni.ini
+conffile = "/etc/openoni.ini"
+if os.path.isfile(conffile):
+  conf = ConfigParser.RawConfigParser(allow_no_value=True)
+  conf.read(conffile)
+
+  if conf.has_section("database"):
+    override_db(conf)
+
+  if conf.has_section("solr"):
+    override_solr(conf)
+
+  if conf.has_section("secrets"):
+    override_secrets(conf)
