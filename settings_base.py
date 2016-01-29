959d745fd5deff7f4610df3faf370bc7a99c0b60
Jeremy Echols <jechols@uoregon.edu>
2016-01-29 15:49:27 -0800

    Replace all thumb/medium image URIs in templates

--- settings_base.py
+++ settings_base.py
@@ -148,7 +148,7 @@ RESIZE_SERVER = "http://example.com/images/resize"
 TILE_SERVER = "http://example.com/images/tile"
 
 # How big should thumbnails be?
-THUMBNAIL_WIDTH = 360
+THUMBNAIL_WIDTH = 240
 
 # These determine the life of various caches (via cache_page)
 DEFAULT_TTL_SECONDS = _ONEDAY
