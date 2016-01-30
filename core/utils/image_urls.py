bf96d6ae826316667b37d8b6944d213cc7641ee1
Jeremy Echols <jechols@uoregon.edu>
2016-01-29 16:23:48 -0800

    Add helper for getting printable images

--- core/utils/image_urls.py
+++ core/utils/image_urls.py
@@ -7,3 +7,7 @@ def thumb_image_url(page):
 def medium_image_url(page):
     # TODO: FIX THIS!  Use a real URL lib, use IIIF, etc
     return settings.RESIZE_SERVER + "/" + page.relative_image_path + "/%dx%d" % (550, 0)
+
+def specific_tile_url(page, w, h, x1, y1, x2, y2):
+    # TODO: FIX THIS!  Use a real URL lib, use IIIF, etc
+    return settings.TILE_SERVER + "/" + page.relative_image_path + "/image_%sx%s_from_%s,%s_to_%s,%s.jpg" % (w, h, x1, y1, x2, y2)
