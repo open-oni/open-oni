49e4f7b1b351201619f3560eeb50ac328873c54c
Jeremy Echols <jechols@uoregon.edu>
2016-04-21 16:53:33 -0700

    Fix IIIF URL generation
    - Uses %d in all format strings so we can't accidentally get a string in
      one without a warning/error
    - Uses /full/... for thumbnail and medium URL

--- core/utils/image_urls.py
+++ core/utils/image_urls.py
@@ -3,14 +3,14 @@ from django.conf import settings
 from django.utils.http import urlquote
 
 def thumb_image_url(page):
-    return tile_server_for_page(page) + "/0,0,%s,%s/%s,/0/default.jpg" % (page.jp2_width, page.jp2_length, settings.THUMBNAIL_WIDTH)
+    return tile_server_for_page(page) + "/full/%d,/0/default.jpg" % settings.THUMBNAIL_WIDTH
 
 def medium_image_url(page):
-    return tile_server_for_page(page) + "/0,0,%s,%s/%s,/0/default.jpg" % (page.jp2_width, page.jp2_length, 550)
+    return tile_server_for_page(page) + "/full/550,/0/default.jpg"
 
 def specific_tile_url(page, w, h, x1, y1, x2, y2):
     x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
-    return tile_server_for_page(page) + "/%s,%s,%s,%s/%s,%s/0/default.jpg" % (x1, y1, x2-x1, y2-y1, w, h)
+    return tile_server_for_page(page) + "/%d,%d,%d,%d/%d,%d/0/default.jpg" % (x1, y1, x2-x1, y2-y1, w, h)
     
 def tile_server_for_page(page):
     return settings.IIIF_SERVER + "/" + urlquote(page.relative_image_path, safe="")
