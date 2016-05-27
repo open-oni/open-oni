7b5b237eac39ace4d96de814161554f5fd261b1d
Jeremy Echols <jechols@uoregon.edu>
2016-05-27 15:52:41 -0700

    Improve test runner
    - Allows customizing the app to test
    - Allows increasing verbosity
    - Finds our test pattern instead of the default
    - Doesn't delete test db between runs, so testing is much faster

--- docker/test.sh
+++ docker/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-APP=core
+APP=${1:-core}
+VERBOSITY=${2:-0}
+
+echo "Testing $APP"
 
 source /opt/openoni/ENV/bin/activate
 
 cd /opt/openoni
-django-admin.py test $APP
+django-admin.py test $APP --keepdb --pattern="*_tests.py" --verbosity=$VERBOSITY
