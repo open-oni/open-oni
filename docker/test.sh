4b5c989f7dcf7f95143750d08c4729ceb9ae0cd5
Jeremy Echols <jechols@uoregon.edu>
2016-07-13 07:59:55 -0700

    Remove "cd /opt/openoni" from scripts
    This isn't necessary; the container starts in /opt/openoni

--- docker/test.sh
+++ docker/test.sh
@@ -6,6 +6,4 @@ VERBOSITY=${2:-0}
 echo "Testing $APP"
 
 source /opt/openoni/ENV/bin/activate
-
-cd /opt/openoni
 django-admin.py test $APP --keepdb --pattern="*_tests.py" --verbosity=$VERBOSITY --settings=openoni.test_settings
