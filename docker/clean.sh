31c66219d2240b1c785c0e49c5a9c7a4f9db9bd7
Jeremy Echols <jechols@uoregon.edu>
2016-07-13 10:12:04 -0700

    Add --nuclear flag option to ./docker/clean.sh

--- docker/clean.sh
+++ docker/clean.sh
@@ -11,4 +11,11 @@ for service in openoni-dev openoni-dev-mysql openoni-dev-solr openoni-dev-rais; 
   docker rm $service
 done
 
+nuclear=${1:-}
+if [ "$nuclear" == "--nuclear" ]; then
+  echo "OMG GOING NUCLEAR!  All your Solr and MySQL data is GONE."
+  docker rm openoni-dev-data-mysql openoni-dev-data-solr
+  docker volume rm $(docker volume ls -qf dangling=true)
+fi
+
 echo "Run ./docker/dev.sh to set your environment back up"
