974139d8a24d689da505c880e01b764f62875dbd
Jeremy Echols <jechols@uoregon.edu>
2016-07-14 13:35:34 -0700

    Log startup tasks better
    Why?  Because I'm super smart.

--- docker/startup.sh
+++ docker/startup.sh
@@ -26,8 +26,14 @@ service apache2 reload
 
 cd /opt/openoni
 source ENV/bin/activate
+
+echo "Migrating database" >&2
 django-admin.py migrate
+
+echo "Running openoni_sync" >&2
 django-admin.py openoni_sync
+
+echo "Running collectstatis" >&2
 django-admin.py collectstatic --noinput
 
 # Remove any pre-existing PID file which prevents Apache from starting
