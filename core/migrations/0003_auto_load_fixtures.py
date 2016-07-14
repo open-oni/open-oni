e5270f496f60b1d8be09729187a800b2e6de77d9
Jessica Dussault <jdussault4@gmail.com>
2016-07-14 09:25:32 -0500

    Pulls initial_data into separate fixture types for db seeding

--- core/migrations/0003_auto_load_fixtures.py
+++ core/migrations/0003_auto_load_fixtures.py
@@ -7,7 +7,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     def load_data(apps, schema_editor):
-        call_command("loaddata", "initial_data.json")
+        call_command("loaddata", "awardees.json")
+        call_command("loaddata", "countries.json")
+        call_command("loaddata", "material_types.json")
 
     dependencies = [
         ('core', '0002_auto_20160713_1509'),
