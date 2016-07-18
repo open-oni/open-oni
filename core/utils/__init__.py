49f869ceaeb27253cd3df244d01bd59a38d4db57
lsato <lsato@uoregon.edu>
2016-07-18 10:43:47 -0700

    add copyright to models

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,8 +1,15 @@
 # __init__.py
 
+import datetime
 from django.utils import datetime_safe
 
 def strftime(d, fmt):
     """works with datetimes with years less than 1900
     """
     return datetime_safe.new_datetime(d).strftime(fmt)
+
+def strdate_to_ordinal(date):
+    date_arr = date.split('-')
+    date_obj = datetime.date(int(date_arr[0]), int(date_arr[1]), int(date_arr[2]))
+    return date_obj.toordinal()
+
