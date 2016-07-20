12978a108616acbd5671c8c5c9d2aac6271548f4
Jeremy Echols <jechols@uoregon.edu>
2016-07-20 08:29:56 -0700

    Remove unnecessary shortcut scripts
    Neither of these two are really shortcutting anything now that we have
    the "manage" shortcut

--- docker/collect_static.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/openoni/ENV/bin/activate
-/opt/openoni/manage.py collectstatic --noinput
--- docker/purge_batch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/openoni/ENV/bin/activate
-/opt/openoni/manage.py purge_batch $1
