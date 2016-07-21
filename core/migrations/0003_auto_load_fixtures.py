0c21335e05d52af3cc17a2f863842ab2f86503fa
Jeremy Echols <jechols@uoregon.edu>
2016-07-21 11:44:16 -0700

    Avoid using call_command in migration
    There is an issue with django-nose and migrations that do this.  It's
    been sitting for like a year, so this seems like our best option.

--- core/migrations/0003_auto_load_fixtures.py
+++ core/migrations/0003_auto_load_fixtures.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core import serializers
 from django.core.management import call_command
 from django.db import migrations, models
+import os
 
+fixture_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../fixtures'))
+
+def load_fixture(fixture_filename):
+    fixture_file = os.path.join(fixture_dir, fixture_filename)
+
+    fixture = open(fixture_file, 'rb')
+    objects = serializers.deserialize('json', fixture, ignorenonexistent=True)
+    for obj in objects:
+        obj.save()
+    fixture.close()
 
 class Migration(migrations.Migration):
-
     def load_data(apps, schema_editor):
-        call_command("loaddata", "awardees.json")
-        call_command("loaddata", "countries.json")
-        call_command("loaddata", "material_types.json")
-        call_command("loaddata", "languages.json")
-        call_command("loaddata", "institutions.json")
-        call_command("loaddata", "ethnicities.json")
-        call_command("loaddata", "labor_presses.json")
-        call_command("loaddata", "countries.json")
+        load_fixture("awardees.json")
+        load_fixture("countries.json")
+        load_fixture("material_types.json")
+        load_fixture("languages.json")
+        load_fixture("institutions.json")
+        load_fixture("ethnicities.json")
+        load_fixture("labor_presses.json")
+        load_fixture("countries.json")
 
     dependencies = [
         ("core", "0002_auto_20160713_1509"),
