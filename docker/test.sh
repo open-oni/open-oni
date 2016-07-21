346014cb9f7b3160a12c4839cfe75db0c1ce2b22
Jeremy Echols <jechols@uoregon.edu>
2016-07-21 13:31:54 -0700

    Improve test shortcut
    - Adds raw coverage report which shows branch coverage
    - Tells devs where to find coverage reports
    - Excludes "*_example.py" from coverage testing

--- docker/test.sh
+++ docker/test.sh
@@ -4,6 +4,11 @@ echo "Testing"
 
 cd /opt/openoni
 source ENV/bin/activate
-coverage run --source="." manage.py test --keepdb --settings=onisite.test_settings
+coverage run --source="." --branch manage.py test --keepdb --settings=onisite.test_settings
 rm -rf static/cov
 coverage html -d static/cov/ --omit="ENV/*"
+coverage report --omit="ENV/*,*_example.py" >static/cov/raw.txt
+
+echo
+echo "Visit $APP_URL/coverage to see a coverage report"
+echo "Visit $APP_URL/coverage/raw.txt to see the raw report, including branch information"
