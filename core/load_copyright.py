a464d6afe2adbe316e285cd043f156752616ae0a
lsato <lsato@uoregon.edu>
2016-07-22 11:22:20 -0700

    change load copyright stuff to commands

--- core/load_copyright.py
+++ core/load_copyright.py
@@ -1,38 +1,23 @@
-import getopt
+import logging
 import sys
-from openoni.core.models import Copyright
+from core.models import Copyright
 
-def main(argv):
-  inputfile = ''
-  try:
-    opts, args = getopt.getopt(argv,"hi:",["ifile="])
-  except getopt.GetoptError:
-    print 'load_copyright.py -i <inputfile>'
-    sys.exit(2)
-  try:
-    for opt, arg in opts:
-      if opt == '-h':
-        print 'load_copyright.py -i <inputfile>'
-        sys.exit()
-      elif opt in ("-i", "--ifile"):
-        inputfile = arg
-      else:
-        assert False, "unhandled option"
+def loadCopyright(inputfilename):
 
-    f = open(inputfile, 'r')
-    for line in f:
-      arr = line.split("\t")
-      if len(arr) == 2:
-        record = Copyright()
-        record.uri = arr[0]
-        record.label =arr[1].strip()
-        record.save()
+    _logger = logging.getLogger(__name__)
+
+    try:
+        f = open(inputfilename, 'r')
+
+        for line in f:
+            arr = line.split("\t")
+            if len(arr) == 2:
+              record = Copyright()
+              record.uri = arr[0]
+              record.label =arr[1].strip()
+              record.save()
+    except Exception as e:
+        _logger.exception(e)
 
     f.close()
-
-  except:
-    print (sys.exc_info()[0])
-
-if __name__ == "__main__":
-  main(sys.argv[1:])
 
--- core/load_copyright_map.py
+++ core/load_copyright_map.py
@@ -1,59 +1,34 @@
-import getopt
+import logging
 import sys
 import datetime
-import pdb
-from openoni.core.models import Copyright
-from openoni.core.models import LccnDateCopyright
+from core.models import Copyright
+from core.models import LccnDateCopyright
 
 def check_uri(_uri):
-  result = Copyright.objects.filter(uri=_uri)
-  return result.exists()
+    try:
+        result = Copyright.objects.filter(uri=_uri)
+        return result.exists()
+    except Exception as e:
+        _logger.exception(e)
 
-def strdate_to_ordinal(date):
-  date_arr = date.split('-')
-  date_obj = datetime.date(int(date_arr[0]), int(date_arr[1]), int(date_arr[2]))
-  return date_obj.toordinal()
+def loadCopyrightMap(inputfilename):
 
-def main(argv):
-  inputfile = ''
-  try:
-    opts, args = getopt.getopt(argv,"hi:",["ifile="])
-  except getopt.GetoptError:
-    print 'load_copyright_map.py -i <inputfile>'
-    sys.exit(2)
-  try:
-    for opt, arg in opts:
-      if opt == '-h':
-        print 'load_copyright_map.py -i <inputfile>'
-        sys.exit()
-      elif opt in ("-i", "--ifile"):
-        inputfile = arg
-      else:
-        assert False, "unhandled option"
+    _logger = logging.getLogger(__name__)
+    try:
+        f = open(inputfilename, 'r')
+        for line in f:
+            arr = line.split("\t")
+            if len(arr) == 4:
+                if check_uri(arr[3].strip()):
+                    record = LccnDateCopyright()
+                    record.lccn = arr[0]
+                    record.start_date = (arr[1])
+                    record.end_date = (arr[2])
+                    result = Copyright.objects.filter(uri=arr[3].strip())
+                    record.uri = result[0]
+                    record.save()
 
-    f = open(inputfile, 'r')
-    for line in f:
-      arr = line.split("\t")
-      if len(arr) == 4:
-        if check_uri(arr[3].strip()):
-          record = LccnDateCopyright()
-          record.lccn = arr[0]
-          record.start_date = strdate_to_ordinal(arr[1])
-          record.end_date = strdate_to_ordinal(arr[2])
-          result = Copyright.objects.filter(uri=arr[3].strip())
-          #pdb.set_trace()
-          record.uri = result[0]
-          record.save()
-        else:
-          raise ValueError("Copyright uri not in system", arr[3])
+    except Exception as e:
+       _logger.exception(e)
 
     f.close()
-  except ValueError as err:
-    print(err.args)
-  except:
-    print (sys.exc_info()[0])
-
-if __name__ == "__main__":
-  main(sys.argv[1:])
-
-
