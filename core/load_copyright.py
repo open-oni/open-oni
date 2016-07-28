e86df135540ef4bd724595f0b61bcde8e8fa8f0b
lsato <lsato@uoregon.edu>
2016-07-28 15:07:23 -0700

    rework error handling

--- core/load_copyright.py
+++ core/load_copyright.py
@@ -1,23 +1,43 @@
 import logging
 import sys
 from core.models import Copyright
+from django.db import transaction
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 def loadCopyright(inputfilename):
 
     _logger = logging.getLogger(__name__)
+    val = URLValidator()
 
     try:
         f = open(inputfilename, 'r')
-
-        for line in f:
-            arr = line.split("\t")
-            if len(arr) == 2:
-              record = Copyright()
-              record.uri = arr[0]
-              record.label =arr[1].strip()
-              record.save()
-    except Exception as e:
+    except IOError as e:
         _logger.exception(e)
+        sys.exit(1)
+
+    for line in f:
+        arr = line.split("\t")
+        if len(arr) == 2:
+            try:
+                val(arr[0])
+            except ValidationError as e:
+                _logger.exception(e)
+                sys.exit(1)
+            record = Copyright()
+            record.uri = arr[0]
+            record.label =arr[1].strip()
+            try:
+                with transaction.atomic():
+                    record.save()
+
+            except Exception as e:
+                _logger.exception(e)
+                sys.exit(1)
+
+        elif line and line.strip():
+            _logger.error("There is an error in the input: " + line)
+            sys.exit(1)
 
     f.close()
 
--- core/load_copyright_map.py
+++ core/load_copyright_map.py
@@ -1,34 +1,60 @@
 import logging
 import sys
 import datetime
+import time
+import pdb
 from core.models import Copyright
 from core.models import LccnDateCopyright
+from django.db import transaction
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
-def check_uri(_uri):
-    try:
-        result = Copyright.objects.filter(uri=_uri)
-        return result.exists()
-    except Exception as e:
-        _logger.exception(e)
 
 def loadCopyrightMap(inputfilename):
 
     _logger = logging.getLogger(__name__)
+    val = URLValidator()
+
     try:
         f = open(inputfilename, 'r')
-        for line in f:
-            arr = line.split("\t")
-            if len(arr) == 4:
-                if check_uri(arr[3].strip()):
-                    record = LccnDateCopyright()
-                    record.lccn = arr[0]
-                    record.start_date = (arr[1])
-                    record.end_date = (arr[2])
-                    result = Copyright.objects.filter(uri=arr[3].strip())
-                    record.uri = result[0]
+    except IOError as e:
+        _logger.exception(e)
+        sys.exit(1)
+
+    for line in f:
+        arr = line.split("\t")
+        if len(arr) == 4:
+            try:
+                val(arr[3].strip())
+            except ValidationError as e:
+                _logger.exception(e)
+                sys.exit(1)
+            try:
+                sdate = datetime.datetime.strptime(arr[1], "%Y-%m-%d").date()
+                edate = datetime.datetime.strptime(arr[2], "%Y-%m-%d").date()
+            except ValueError as e:
+                _logger.exception(e)
+                sys.exit(1)
+
+            record = LccnDateCopyright()
+            record.lccn = arr[0]
+            record.start_date = sdate
+            record.end_date = edate
+
+            try:
+                result = Copyright.objects.filter(uri=arr[3].strip())
+                record.copyright = result[0]
+            except Exception as e:
+                _logger.exception(e)
+                sys.exit(1)
+
+            try:
+                with transaction.atomic():
                     record.save()
-
-    except Exception as e:
-       _logger.exception(e)
-
+            except Exception as e:
+                _logger.exception(e)
+                sys.exit(1)
+        elif line and line.strip():
+            _logger.error("There is an error in the input: " + line)
+            sys.exit(1)
     f.close()
