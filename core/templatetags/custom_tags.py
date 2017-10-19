d0b6498b7f97d96cd36e1837583381cfb1ffade0
Jeremy Echols <jechols@uoregon.edu>
2017-10-18 17:17:45 -0700

    Make titles removable from filtering

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -16,3 +16,17 @@ def remove_param(context, *args):
         if arg in params:
             del params[arg]
     return urlencode(params)
+
+@register.simple_tag(takes_context=True)
+def remove_param_value(context, key, value):
+    """Given a request.GET or .POST object, shallow clone,
+    and remove a single key/value pair"""
+    request = context["request"]
+    params = request.GET.copy()
+    if key in params:
+        args = params.getlist(key)
+        if value in args:
+            args.remove(value)
+            params.setlist(key, args)
+
+    return params.urlencode()
