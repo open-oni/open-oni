d83f8bab88fb85aa925cbc4f9090e6df19338f3f
Jeremy Echols <jechols@uoregon.edu>
2018-01-24 12:33:25 -0800

    Ensure pip installs known-safe dependencies
    Sets up a requirements.lock file with values from `pip freeze` which are
    known to work on a staging system, and uses that file in the
    `pip-install.sh` shortcut

--- docker/pip-install.sh
+++ docker/pip-install.sh
@@ -4,7 +4,7 @@
 virtualenv ENV
 source ENV/bin/activate
 pip install -U setuptools
-pip install -r requirements.pip --allow-all-external
+pip install -r requirements.lock --allow-all-external
 
 install -d /opt/openoni/static
 install -d /opt/openoni/.python-eggs
