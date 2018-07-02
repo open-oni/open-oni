f856ef6da9d197f05939fc4f1ec1395d09ce83fa
John Scancella <jsca@loc.gov>
2018-07-02 08:57:15 -0400

    converted dump_ocr to no longer use celery

--- core/management/commands/dump_ocr.py
+++ core/management/commands/dump_ocr.py
@@ -1,10 +1,16 @@
+import logging
 import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from core.models import Batch
-from core.tasks import dump_ocr
+from core.management.commands import configure_logging
+
+from core.models import Batch, OcrDump
+
+configure_logging("dump_ocr_logging.config", "dump_ocr.log")
+_logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     help = "looks for batches that need to have ocr dump files created"
@@ -14,5 +20,13 @@ class Command(BaseCommand):
             os.makedirs(settings.OCR_DUMP_STORAGE)
 
         for batch in Batch.objects.filter(ocr_dump__isnull=True):
-            print "queueing %s for ocr dump" % batch
-            dump_ocr.delay(batch.name)
+            _logger.info("starting to dump ocr for %s", batch)
+            try:
+                if batch.ocr_dump:
+                    _logger.info("Ocr is already generated for %s", batch)
+                    continue
+            except OcrDump.DoesNotExist:
+                pass
+
+            dump = OcrDump.new_from_batch(batch)
+            _logger.info("created ocr dump %s for %s", dump, batch)
