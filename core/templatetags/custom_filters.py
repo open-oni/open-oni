670efbfe572a00073476909006620f433b7bee79
Jessica Dussault <jdussault4@gmail.com>
2018-08-27 13:36:41 -0500

    alphabetizes custom_filters

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -7,21 +7,22 @@ from core.utils import url
 from core.utils.utils import label
 
 
-
 register = template.Library()
 
-@register.filter(name='rfc3339')
-def rfc3339_filter(d):
-    return rfc3339(d)
-
-@register.filter(name='pack_url')
-def pack_url(value, default='-'):
-    return url.pack_url_path(value, default)
 
 @register.filter(name='label')
 def _label(value):
     return label(value)
 
+
+@register.filter(name='pack_url')
+def pack_url(value, default='-'):
+    return url.pack_url_path(value, default)
+
+
+@register.filter(name='rfc3339')
+def rfc3339_filter(d):
+    return rfc3339(d)
 
 
 @register.filter
