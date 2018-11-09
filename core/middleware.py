c51d7235d47690884da00a794c3f522933579925
Greg Tunink <techgique@gmail.com>
2018-11-09 17:07:56 -0600

    Fix core urls.py and custom middleware
    Updated for Django 1.10
    https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware

--- core/middleware.py
+++ core/middleware.py
@@ -3,9 +3,10 @@ import os
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils.cache import add_never_cache_headers
+from django.utils.deprecation import MiddlewareMixin
 
 
-class DisableClientSideCachingMiddleware(object):
+class DisableClientSideCachingMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         add_never_cache_headers(response)
         return response
@@ -15,27 +16,25 @@ class HttpResponseServiceUnavailable(HttpResponse):
     status_code = 503
 
 
-class TooBusyMiddleware(object):
-
+class TooBusyMiddleware(MiddlewareMixin):
     def process_request(self, request):
         one, five, fifteen = os.getloadavg()
         if one > settings.TOO_BUSY_LOAD_AVERAGE:
             return HttpResponseServiceUnavailable("""
 <!doctype html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <title>Server Too Busy</title> 
-  <style></style>
-</head>
-<body>
-     <article>
-	  <h1>Server Too Busy</h1>
-	   <div>
-            <p>Please try your request again shortly.</p>
-	   </div>
-     </article>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <title>Server Too Busy</title>
+  </head>
+  <body>
+    <article>
+      <h1>Server Too Busy</h1>
+      <div>
+        <p>Please try your request again shortly.</p>
+      </div>
+    </article>
+  </body>
 </html>
 """)
         return None
