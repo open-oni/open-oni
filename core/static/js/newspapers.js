a9fc1ad2e2b45a1b4e74f01a995c9dd3153e2f87
Greg Tunink <techgique@gmail.com>
2019-07-18 17:11:36 -0500

    Move newspapers page JS to external file

---
+++ core/static/js/newspapers.js
@@ -0,0 +1,53 @@
+// Customization for tablesorter (jquery.tablsorter.min.js)
+$.tablesorter.addParser({
+    // set a unique id
+    id: 'titles',
+    is: function(s) {
+        // return false so this parser is not auto detected
+        return false;
+    },
+    format: function(s) {
+        // Format your data for normalization
+        s = $.trim(s)  // Make sure there are no leading spaces
+        s = s.toLowerCase();
+        s = s.replace(/^the /, '');
+        s = s.replace(/^an? /, '');
+        return s;
+    },
+    type: 'text'
+});
+
+$(document).ready(function() {
+    // Apply tablesorter to #newspapers table
+    $("#newspapers").tablesorter({
+        headers: {
+            // When sorting by titles, use the title formatter
+            // so we skip "the", "a", and "an"
+            '.sort-titles': { sorter: 'titles' },
+
+            // Disable sorting, e.g. on the "browse issues" link
+            '.sort-off': { sorter: false },
+        },
+        widgets: ['zebra'],
+
+        // Default to sorting by first column
+        sortList: [[0,0]]
+    });
+
+    // Filter text of each row in #newspapers table
+    $("#filterTitles").keyup(function() {
+        var searchText = $(this).val().toLowerCase();
+        $.each($("#newspapers tbody th"), function() {
+            if($(this).text().toLowerCase().indexOf(searchText) === -1)
+                $(this).parent().hide();
+            else
+               $(this).parent().show();
+        });
+    });
+    $("#resetTitles").click(function() {
+        $("#filterTitles").val("");
+        $.each($("#newspapers tbody tr"), function() {
+            $(this).show();
+        });
+    });
+});
