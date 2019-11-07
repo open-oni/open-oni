350c6e298ae8e8d4022bbfd3a75c0cfb8cd0e920
Greg Tunink <techgique@gmail.com>
2019-11-07 09:17:59 -0600

    Simplify settings files; Add more env var control
    - Remove separate dev and prod settings files
    - Move settings_base.py into settings.py
    - Streamline settings_local.example.py
        - Add env var config with working defaults if no env vars set
        - Move Django error reporting emails to docs as optional
        - Remove IS_PRODUCTION and rely on DEBUG
    - Remove lines in core/context_processors.py for unused settings
    - Update docker-compose.yml to pass through env vars
        - Make env_file .env explicit so people are aware of it
        - Ignore .env
        - Add a .env.example file with same defaults as no .env file
        - Fix syntax for default values when env var is present but empty
    - Remove link to non-existent Django section in docs/openoni.md

--- onisite/settings.py
+++ onisite/settings.py
@@ -1,2 +1,128 @@
-from settings_base import *
+import os
+
+from django_defaults import *
+
+################################################################
+# DJANGO CUSTOMIZATIONS
+################################################################
+# Enable browser XSS protection, MIME-type sniff prevention headers,
+# and disable framing / embedding
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Site id differentiates DB data if multiple sites use the same database
+SITE_ID = 1
+
+# Directory path to static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'compiled')
+
+# Template configuration (1.8+)
+TEMPLATES = [
+    {
+        # Whether engine looks inside application directories for templates
+        'APP_DIRS': True,
+
+        # Template engine
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+
+        # Template-containing directories
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+
+        'OPTIONS': {
+            # https://docs.djangoproject.com/en/1.9/topics/templates/#module-django.template.backends.django
+            
+            # Callables which alter the request context
+            'context_processors': [
+                # Default
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+#                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+
+                # Open ONI
+                'django.template.context_processors.csrf',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'core.context_processors.extra_request_info',
+            ],
+        },
+    },
+]
+
+
+################################################################
+# DEFAULT OPEN-ONI SETTINGS
+################################################################
+# These determine the life of various caches (via cache_page)
+API_TTL_SECONDS = 60 * 60  # One hour
+DEFAULT_TTL_SECONDS = API_TTL_SECONDS * 24  # One day
+FEED_TTL_SECONDS = DEFAULT_TTL_SECONDS * 7  # One week
+PAGE_IMAGE_TTL_SECONDS = FEED_TTL_SECONDS * 2  # Two weeks
+
+# List of breadcrumbs that will be shown on all pages
+BASE_CRUMBS = [{'label':'Home', 'href': '/'}]
+
+# Batch and title management log directory path
+LOG_LOCATION = os.path.join(BASE_DIR, 'log')
+
+MARC_RETRIEVAL_URLFORMAT = 'https://chroniclingamerica.loc.gov/lccn/%s/marc.xml'
+
+# Display newspaper titles with medium ("volume", "microform") when available
+TITLE_DISPLAY_MEDIUM = False
+
+
+################################################################
+# DEFAULT IIIF SETTINGS
+################################################################
+# How big should thumbnails be?
+THUMBNAIL_WIDTH = 240
+
+# Use JP2 file paths with RAIS rather than TIFF file paths
+USE_TIFF = False
+
+
+################################################################
+# DEFAULT SOLR SETTINGS
+################################################################
+# Languages solr uses
+SOLR_LANGUAGES = (
+    'ara',
+    'arm',
+    'baq',
+    'bul',
+    'cze',
+    'dan',
+    'dut',
+    'eng',
+    'fin',
+    'fre',
+    'ger',
+    'gle',
+    'gre',
+    'hin',
+    'hun',
+    'ind',
+    'ita',
+    'jpn',
+    'lav',
+    'nor',
+    'per',
+    'por',
+    'rum',
+    'rus',
+    'spa',
+    'swe',
+    'tha',
+    'tur',
+)
+
+################################################################
+# LOCAL SETTINGS
+################################################################
 from settings_local import *
+
