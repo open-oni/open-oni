e26600896c9e60c817d5b69b6915e791891207cd
Jeremy Echols <jechols@uoregon.edu>
2026-03-26 08:01:28 -0700

    Refactor container services / definitions / etc
    This is a massive refactor:
    
    - Renames compose files, using orchestration-agnostic naming convention.
      `compose.yaml` is specifically recommended as the "preferred" name
    - Removes all forced ports from base compose file
      - This is insecure (port with no IP binding) and not good practice
    - In overrides, exposes services only on 127.0.0.1 for security.
      Accidental copy/paste won't expose something like mariadb to the
      outside world.
    - Removes "version" from compose: this is obsolete
    - Removes all direct host filesystem mounts in base compose file
    - Builds services from local Dockerfiles instead of just pulling the
      default image for a service (solr, mariadb, etc.). This lets us bake
      config and commands that never change directly into various images.
    - Adds health check to rdbms to eliminate custom "is db up?" scripting
    - Improves customization: `.env` for all per-environment settings,
      compose override strictly for orchestration concerns like how to
      define various volumes
    - Various environment settings are now shared and validated via more
      modern compose spec options, e.g.:
      - `ONI_BASE_URL` is now used to prefix `ONI_IIIF_URL` in addition to
        being shared between multiple services
      - `DB_PASSWORD` is shared between the rdbms and web services, and must
        be set for the stack to start
    - Defines and uses volumes better
      - e.g., "rais", "app", and "web" all need the `oni-data` volume, but
        only `app` needs to write to it
      - The override example now redefines volumes rather than in-lining
        filesystem mounts, keeping the integrity of read-only volumes
    - Python image avoids virtual environment, greatly simplifying the "what
      do we do about ENV" debate
    - Containerization is aimed more for production use. Dev use will be
      tougher, but we devs can handle it, while prod users should be able to
      make it work with less effort.
    - Replaces single-service Apache definition with two-service setup:
      Caddy (`web` service) is exposed to the host and serves static files,
      but doesn't see the code directly; gunicorn (`app` service) serves the
      django code and is the only service where management commands can run
    - New `.dockerignore` ignores everything by default except what's
      *absolutely required* for images to build and ONI to run. Built image
      should never accidentally end up with batches, secrets, etc.

--- docker/load_batch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-manage load_batch /opt/openoni/data/batches/$1
--- docker/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /_startup_lib.sh
-
-verify_config
-setup_database
-setup_index
-prep_webserver
-run_apache
